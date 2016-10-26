8b69cb789287f44d7c6df68d00b771b287abc904
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-10-26 20:11:46 +0900

    imageのカラムがtextだったのでstringに修正

--- db/migrate/20161026110458_create_messages.rb
+++ db/migrate/20161026110458_create_messages.rb
@@ -2,10 +2,10 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text       :body,  null: false
-      t.text       :image
+      t.string     :image
       t.references :user,  foreign_key: true
       t.references :group, foreign_key: true
-      t.timestamps
+      t.timestamp
     end
   end
 end
