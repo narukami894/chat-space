a77dd2211360359a02e4a632fb94b6a3cf0a5225
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-10-26 16:45:38 +0900

    nameのカラム追加

--- db/migrate/20161026073010_devise_create_users.rb
+++ db/migrate/20161026073010_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,                            default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -31,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
