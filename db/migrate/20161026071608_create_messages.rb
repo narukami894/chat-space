d1cca410a359decb634059ae43479978903c9842
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-10-26 16:32:34 +0900

    rails g model group rails g devise:install rails g devise user

--- db/migrate/20161026071608_create_messages.rb
+++ db/migrate/20161026071608_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      body :text
+      image :text
+      user_id :integer, null: false
+      group_id :integer, null: false
       t.timestamps
     end
   end
