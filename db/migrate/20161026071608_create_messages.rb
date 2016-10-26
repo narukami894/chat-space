a7ecb3e39b855640e49b161baadf45d4610f2781
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-10-26 18:39:02 +0900

    null: falseの追加(グループ名、ユーザ名、メッセージのbody) 外部キー制約の追加

--- db/migrate/20161026071608_create_messages.rb
+++ db/migrate/20161026071608_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text       :body
+      t.text       :body,  null: false
       t.text       :image
-      t.integer    :user_id,  null: false
-      t.integer    :group_id, null: false
+      t.references :user,  foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20161026074434_create_groups_users.rb
+++ db/migrate/20161026074434_create_groups_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :groups_users do |t|
-      t.integer    :user_id,  null: false
-      t.integer    :group_id, null: false
+      t.references :user,  foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
