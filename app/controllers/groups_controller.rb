63ac481657e4e26bde9c710f824e15484d60511a
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-06 16:07:57 +0900

    ちゃんとルーティングしたらmessages入れ子にしてしまってform_forが困っているのでmessageのindexだけ動くように

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,5 @@
 class GroupsController < ApplicationController
+  def index
+    @group = Group.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "groups#index"
-  resources :groups, only: [:index, :new, :edit] do
-    resources :messages, only: :index
-  end
+  resources :messages, only: :index
+  root to: "messages#index"
+  # resources :groups, only: [:index, :new, :edit] do
+  #   resources :messages, only: :index
+  # end
 end
