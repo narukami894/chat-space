49be5fd6829de3ce8adfa4c7ed206889e823672c
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-06 16:10:42 +0900

    chatSideだけでレイアウトが綺麗になるかどうか確認するためroutesを一部戻す

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: :index
   root to: "messages#index"
-  # resources :groups, only: [:index, :new, :edit] do
+  resources :groups, only: [:index, :new, :edit]
+  # do
   #   resources :messages, only: :index
   # end
 end
