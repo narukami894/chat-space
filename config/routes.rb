d737aebea198236d8825c5a4314fcfd68b7e07ad
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-07 12:03:52 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
   resources :messages, only: :index
   root to: "messages#index"
   resources :groups, only: [:index, :new, :edit]
-  # do
-  #   resources :messages, only: :index
-  # end
 end
