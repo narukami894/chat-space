10ba0c7db3c4b7ae9789234f1fe6ca1911c1952a
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-12 17:37:52 +0900

    rake routesしたときにいらんルーティングが出てくるのがアレだったので消した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:sessions, :registrations]
   root to: "groups#index"
   resources :groups, only: [:index, :new, :create, :edit] do
     resources :messages, only: [:index, :create]
