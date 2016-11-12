9257738ac733828d53aa85e7cf2454610813b287
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-12 14:42:40 +0900

    ルーティングを決めた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
-  resources :groups, only: [:index, :new, :edit]
-  resources :messages, only: [:index, :create]
+  resources :groups, only: [:index, :new, :create, :edit] do
+    resources :messages, only: [:index, :create]
+  end
 end
