9815f32b5d5ad80934152807beab8f9f3cbf2a3e
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-13 00:03:27 +0900

    ルーティング間違ってた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, only: [:sessions, :registrations]
   root to: "groups#index"
-  resources :groups, only: [:index, :new, :create, :edit] do
+  resources :groups, except: [:destroy, :show] do
     resources :messages, only: [:index, :create]
   end
 end
