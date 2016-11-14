dffb778abdb4e174d89f92b45d37d696e9cfa7ad
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-14 17:31:17 +0900

    ルーティングを設定した

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,39 @@
 class GroupsController < ApplicationController
+
   before_action :authenticate_user!
+  before_action :set_group, only: [:edit, :update]
   layout 'chat', only: :index
+
   def index
     @group = Group.new
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.create(group_params)
+    redirect_to "/groups/#{group.id}/messages"
+  end
+
+  def edit
+  end
+
+  def update
+    @group.update(group_params)
+    redirect_to "/groups/#{@group.id}/messages"
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,9 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   layout 'chat'
   def index
+    @group = Group.find(params[:group_id])
     @message = Message.new
+    @group_id = params[:group_id]
+
   end
 end
