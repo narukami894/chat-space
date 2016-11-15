5f932f084de42b5636aab4c10e04f75831fe4d5c
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-15 17:06:58 +0900

    チェックボックスによるユーザ追加機能実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages
-  validates :name, presence: true
+  validates :title, presence: true
   accepts_nested_attributes_for :group_users
 end
