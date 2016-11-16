b55141aa3afdaa4930b197773f1298a8b481153e
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-16 18:09:09 +0900

    メッセージ投稿機能と表示機能の作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,20 @@ class MessagesController < ApplicationController
     @users = User.all
     @groups = current_user.groups.newly
     @group = Group.find(params[:group_id])
-    @message = Message.new
     @group_id = params[:group_id]
+    @messages = Message.where(group_id: params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+    group = Group.find(params[:group_id])
+    Message.create(message_params)
+    redirect_to group_messages_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
