6f5f53a3baf5881105a3613d4a5394ff94e0c665
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-18 18:33:20 +0900

    deviseでログイン状態になるような設定を書いた

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.extend ControllerMacros, type: :controller
+  config.include ControllerMacros, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 end
