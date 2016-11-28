2745aa941286cedb4fea8d62bb9e684c0dfc0d92
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-28 16:50:43 +0900

    現状

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,32 +1,58 @@
 function result_html(data){
 
   var result = $(
-    "<li class='searchResult__name'>" +
-      data.name +
-    "</li>"
-    );
+                  "<div>" +
+                    "<input type='hidden'>" +
 
-  return result
+                    "<p class='searchResult__name'>" +
+                      data.name +
+                    "</p>" +
+                    "<a>" +
+
+                    "</a>" +
+                  "</div>"
+                );
+
+  return result;
 };
+
+function ajaxSearch() {
+
+  $.ajax({
+    url: '/users',
+    type: 'GET',
+    data: { name: $('input#group_users').val() } ,
+    dataType: 'json'
+  })
+  .done(function(data){
+    console.log(data);
+    data.forEach(function(){
+      $('#searchResult').append(result_html(data));
+    });
+  })
+  .fail(function(data){
+  });
+}
+
+
 
 
 $(function(){
 
-  $('input#group_users').keyup(function(){
+  $('input#group_users').on('change keyup', ajaxSearch);
 
 
-    $.ajax({
-      url: '/users',
-      type: 'GET',
-      data: { users: $('input#group_users').val() },
-      dataType: 'json'
-    })
 
-    .done(function(data) {
-      $('#searchResult').append(result_html(data));
-    })
-    .fail(function(data) {
-    });
+// 追加ボタン
+//   $().on('click', '', function(){
 
-  });
+//   });
+
+// 削除ボタン
+//   $().on('click', '', function(){
+
+//   });
+
+
+
 });
