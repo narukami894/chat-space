808243f014f7d4c7a29a0912e7b8495498b88e28
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-29 20:44:36 +0900

    画像を非同期で送れるようになった

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,11 @@
 function message_html(data){
 
+  if (data.image) {
+    var image = '<br><img src="' + data.image + '">';
+  } else {
+    var image = '';
+    };
+
   var message = $(
     "<li class='chatMessage'>" +
       "<div class= 'chatMessage__header'>" +
@@ -12,30 +18,38 @@ function message_html(data){
       "</div>" +
       "<p class='chatMessage__body'>" +
       data.body +
+      image +
       "</p>" +
-    "</li>" );
+    "</li>"
+                );
 
     return message;
   };
 
 $(function(){
 
-  // $('#message_image').on('change', function(){
-  //   $('form#new_message').submit();
-  // });
+  $('#message_image').on('change', function(){
+    $(this).parents('form#new_message').submit();
+  });
 
   $('form#new_message').submit(function(e) {
     e.preventDefault();
+
+    var fd = new FormData($('form#new_message').get(0));
+
     $.ajax({
       url: './messages.json',
       type: 'POST',
-      data: { message: { body: $('#message_body').val() } },
+      data: fd,
+      processData: false,
+      contentType: false,
       dataType: 'json'
     })
 
     .done(function(json) {
       $('.chatMessages').append(message_html(json));
       $('#message_body').val('');
+      $('#message_image').val('');
     })
     .fail(function(json) {
     });
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates :body, presence: true
 
   def display_time
     updated_at.strftime("%Y-%m-%d %H:%M:%S")
