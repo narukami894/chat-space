8afe06f2490068977e24e500929fdf9018b4cd59
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-11-30 14:51:29 +0900

    文章もしくは画像のバリデーションを設定した

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,14 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
+  validates :body_or_image, presence: true
 
   def display_time
     updated_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  private
+  def body_or_image
+    body.presence or image.presence
   end
 end
