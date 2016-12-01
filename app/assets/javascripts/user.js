3e9b8e4dd1bd6f4a9197c89ad20e613f460b2dec
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-12-01 21:39:09 +0900

    scssの整理、空行と深いインデントの排除、hidden_field_tag, 連想配列

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,58 +1,45 @@
 function result_html(data){
 
-    var result =
-                  "<div class='searchResult'>" +
-                    "<li class='chatGroupForm__field--right__users__user' data-name='" +
-                    data.name +
-                    "'>" +
-                      data.name +
-                      "<a class='chatGroupForm__field--right__users__user__addbtn' >" +
-                        "追加" +
-                      "</a>" +
-                      "<input name='group[user_ids][]' type='hidden' data-id='" +
-                      data.id +
-                       "' value='" +
-                        data.id +
-                      "'>" +
-                    "</li>" +
-                  "</div>"
+  var result =
+    "<div class='searchResult'>" +
+      "<li class='chatGroupForm__field--right__users__user' data-name='" + data.name + "'>" +
+        data.name +
+        "<a class='chatGroupForm__field--right__users__user__addbtn' >" +
+          "追加" +
+        "</a>" +
+        "<input name='group[user_ids][]' type='hidden' data-id='" + data.id + "' value='" + data.id + "'>" +
+      "</li>" +
+    "</div>"
     return result;
-  };
+};
 
-  function ajaxSearch() {
-    $.ajax({
-      url: '/users.json',
-      type: 'GET',
-      data: { name: $('#group_users').val() } ,
-      dataType: 'json'
-    })
-    .done(function(json){
-      json.forEach(function(data){
-        $('#searchResult').html(result_html(data));
+function ajaxSearch() {
+  $.ajax({
+    url: '/users.json',
+    type: 'GET',
+    data: { name: $('#group_users').val() } ,
+    dataType: 'json'
+  })
+  .done(function(json){
+    json.forEach(function(data){
+      $('#searchResult').html(result_html(data));
 
-      });
-    })
-    .fail(function(json){
     });
- };
+  })
+  .fail(function(json){
+  });
+};
 
-      function added_user_html(data_id, data_name){
-        var added_user =
-                    "<li class='chatGroupForm__field--right__users__user' data-name='" +
-                    data_name +
-                    "'>" +
-                      data_name +
-                      "<a class='chatGroupForm__field--right__users__user__deletebtn' >" +
-                        "削除" +
-                      "</a>" +
-                      "<input name='group[user_ids][]' type='hidden' data-id='" +
-                      data_id +
-                       "' value='" +
-                        data_id +
-                      "'>" +
-                    "</li>" +
-                  "</div>"
-      return added_user
+function added_user_html(user){
+  var added_user =
+    "<li class='chatGroupForm__field--right__users__user' data-name='" + user.name + "'>" +
+      user.name +
+      "<a class='chatGroupForm__field--right__users__user__deletebtn' >" +
+        "削除" +
+      "</a>" +
+      "<input name='group[user_ids][]' type='hidden' data-id='" + user.id + "' value='" + user.id + "'>" +
+    "</li>"
+    return added_user
 }
 
 $(function(){
@@ -62,11 +49,10 @@ $(function(){
   $('#searchResult').on('click', '.chatGroupForm__field--right__users__user__addbtn', function(){
 
     var $this = $(this);
-    var data_id = $this.siblings().data("id");
-    var data_name = $this.parent().data("name");
+    var user = { id: $this.siblings().data("id"), name: $this.parent().data("name") }
 
     $(this).parent('.chatGroupForm__field--right__users__user').remove();
-    $('.chatGroupForm__field--right__users').append(added_user_html(data_id, data_name));
+    $('.chatGroupForm__field--right__users').append(added_user_html(user));
   });
 
   $('.chatGroupForm__field--right__users').on('click', '.chatGroupForm__field--right__users__user__deletebtn', function(){
