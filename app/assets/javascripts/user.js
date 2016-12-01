20d2eaa18d5feb2d28098d2d7fb993f050efbabf
YakushijiShunsuke <s.yakushiji@icloud.com>
2016-12-01 15:27:21 +0900

    現状

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,38 +1,21 @@
 function result_html(data){
 
-
-  var result = $(
-                  "<div class='searchResult'>" +
-                    "<input type='hidden'>" +
-
-                    "<p class='searchResult__name'>" +
-                      data.name +
-                    "<a class='searchResult__name__addbtn'>" +
-                      '追加' +
+  var result =
+                "<div class='searchResult'>" +
+                  "<p class='searchResult__name'>" +
+                    data.name +
+                    "<a class='searchResult__name__addbtn' >" +
+                      "追加" +
                     "</a>" +
-                     "</p>" +
-                  "</div>"
-                );
+                  "</p>" +
+                "</div>"
 
   return result;
 };
 
-function add_html(data){
-
-  var add = $(
-                "<li>" +
-                  data.name +
-                "</li>" 
-               )
-  return add;
-};
-
-
-
 
 
 function ajaxSearch() {
-
   $.ajax({
     url: '/users.json',
     type: 'GET',
@@ -51,22 +34,34 @@ function ajaxSearch() {
 
 
 
-$(function(){
+
+$(function(json){
+
+
+
+
+
 
   $('input#group_users').on('change keyup', ajaxSearch);
 
+  function adddeletebtn{
+    var deletebtn =
+                "<a class='searchResult__name__deletebtn'>" +
+                  "削除" +
+                "</a>"
+    return deletebtn;
+  }
 
 
-// 追加ボタン
-  $('searchResult__name__addbtn').on('click', function(){
-    $('.chatGroupForm__field--right__users').append("")
+  $('#searchResult').on('click', '.searchResult__name__addbtn', function() {
+    $(this).parent('.searchResult__name').remove();
+    $('.chatGroupForm__field--right__users__user').append($(this));
 
   });
 
-// 削除ボタン
-//   $().on('click', '', function(){
-
-//   });
+  $('.searchResult__name__deletebtn').on('click', '', function(){
+    $(this).parent().remove();
+  });
 
 
 
